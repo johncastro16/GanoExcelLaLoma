96d470fbbb13584acc16d4b20185716a37c48433
John Castro <johncastro1610@hotmail.com>
2025-01-21 23:23:06 -0500

    creando variables de entorno de las credenciales

--- src/services/googleSheetsService.js
+++ src/services/googleSheetsService.js
@@ -26,12 +26,23 @@ async function addRowToSheet(auth, spreadsheetId, values) {
 const appendToSheet = async (data) => {
     try {
         const auth = new google.auth.GoogleAuth({
-            keyFile: path.join(process.cwd(), 'src/credentials', 'credentials.json'),
+            credentials: {
+                type: process.env.GOOGLE_TYPE,
+                project_id: process.env.GOOGLE_PROJECT_ID,
+                private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+                private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+                client_email: process.env.GOOGLE_CLIENT_EMAIL,
+                client_id: process.env.GOOGLE_CLIENT_ID,
+                auth_uri: process.env.GOOGLE_AUTH_URI,
+                token_uri: process.env.GOOGLE_TOKEN_URI,
+                auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+                client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL,
+              },
             scopes: ['https://www.googleapis.com/auth/spreadsheets']
         });
 
         const authClient = await auth.getClient();
-        const spreadsheetId = '1c9Aa3cs0-7TUviShg0M02pezW11fDUItm0LK-tL9bBc'
+        const spreadsheetId = process.env.SPREADSHEETID
 
         await addRowToSheet(authClient, spreadsheetId, data);
         return 'Datos correctamente agregados'
